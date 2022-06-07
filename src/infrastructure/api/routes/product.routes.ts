760123a122cf24269aabb31c8ee26380b11cac8c
William Trevisan <williamtrev@hotmail.com>
2022-06-07 20:56:39 -0300

    feat: created a product routes

--- src/infrastructure/api/routes/product.routes.ts
+++ src/infrastructure/api/routes/product.routes.ts
@@ -2,8 +2,10 @@ import { Router } from "express";
 
 const productsRoutes = Router();
 
+const createProductController = new CreateProductController();
 const listProductController = new ListProductController();
 
+productsRoutes.post("/", createProductController.handle);
 productsRoutes.get("/", listProductController.handle);
 
 export { productsRoutes };
