c918baed29504e0ca42ab24795e8e08ac0db6608
William Trevisan <williamtrev@hotmail.com>
2022-06-07 17:12:54 -0300

    test: created a should update a customer into a update customer use case tests

---
+++ src/usecase/customer/update/update.customer.unit.spec.ts
@@ -0,0 +1,36 @@
+import { CustomerFactory } from "../../../domain/customer/factory/customer.factory";
+import { Address } from "../../../domain/customer/value_object/address";
+
+const customer = CustomerFactory.createWithAddressAndActive(
+  "John",
+  new Address("Street name", 1, "zipcode", "City name")
+);
+
+const input = {
+  id: customer.id,
+  name: "Customer name updated",
+  address: {
+    street: "Street name updated",
+    number: 2,
+    zip: "Zipcode updated",
+    city: "City name updated",
+  },
+};
+
+const customerRepository = () => {
+  return {
+    create: jest.fn(),
+    update: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(customer)),
+    findAll: jest.fn(),
+  };
+};
+
+describe("Update customer use case unit test", () => {
+  it("should update a customer", async () => {
+    const updateCustomerUseCase = new UpdateCustomerUseCase(customerRepository);
+    const output = await updateCustomerUseCase.execute(input);
+
+    expect(output).toEqual(input);
+  });
+});
