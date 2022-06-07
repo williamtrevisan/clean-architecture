cbef13a83c578c5235e0ee47a26b50524d892a95
William Trevisan <williamtrev@hotmail.com>
2022-06-07 18:30:10 -0300

    feat: created a input and output dto for find product use case

---
+++ src/usecase/product/find/find.product.dto.ts
@@ -0,0 +1,11 @@
+interface InputCreateProductDTO {
+  id: string;
+}
+
+interface OutputCreateProductDTO {
+  id: string;
+  name: string;
+  price: number;
+}
+
+export { InputCreateProductDTO, OutputCreateProductDTO };
