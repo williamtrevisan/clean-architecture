cda76556195efff318da2f3b1f0b448eed0af5e3
William Trevisan <williamtrev@hotmail.com>
2022-06-07 17:21:33 -0300

    feat: created a input and output dto for list customer use case

---
+++ src/usecase/customer/list/list.customer.dto.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface InputListCustomerDTO {}
+
+type Customer = {
+  id: string;
+  name: string;
+  address: {
+    street: string;
+    number: number;
+    zip: string;
+    city: string;
+  };
+};
+
+interface OutputListCustomerDTO {
+  customers: Customer[];
+}
+
+export { InputListCustomerDTO, OutputListCustomerDTO };
