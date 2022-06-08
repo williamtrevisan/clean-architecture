4b171a63717ca718bdc714edde06d42b1284beb7
William Trevisan <williamtrev@hotmail.com>
2022-06-07 21:00:34 -0300

    feat: created a CreateProductController

---
+++ src/infrastructure/api/controller/product/create/create.product.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+
+import { CreateProductUseCase } from "../../../../../usecase/product/create/create.product.usecase";
+import { ProductRepository } from "../../../../product/repository/sequelize/product.repository";
+
+class CreateCustomerController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    try {
+      const { name, price } = request.body;
+
+      const createProductUseCase = new CreateProductUseCase(
+        new ProductRepository()
+      );
+
+      const product = await createProductUseCase.execute({
+        name,
+        price,
+      });
+
+      return response.status(201).send(product);
+    } catch (error) {
+      return response.status(500).send(error);
+    }
+  }
+}
+
+export { CreateCustomerController };
